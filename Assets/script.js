594c57870bf8032be173affe71de1c29a816bbef
Slender James <114044205+jperalta24@users.noreply.github.com>
2022-12-14 22:27:52 -0800

    added global variables to javascript

--- Assets/script.js
+++ Assets/script.js
@@ -1,15 +1,39 @@
+// ****Global Variables*****\\
 let weatherAPIkey = "4ac77901699e70e15fcbbfca21acc34c";
-let city = "Seattle";
+let city ="";
 let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=4ac77901699e70e15fcbbfca21acc34c";
 
+// find all elements where I need to insert weather data
+let searchButton = $("#searchButton");
+let searchInput = $("#searchCity");
+let searchHistory = $("#cityHistory");
+let currentCity = $("#currentCity");
+let currentWeather =$("#currentWeatherInfo");
+let temp = $(".cityTemp");
+let wind = $(".cityWind");
+let humidity = $(".cityHumidity");
+
+let searchCity = ()=>{
+let city = searchInput.val();
+console.log(city);
+
+getCity();
+}
+
+ let getCity = ()=>{
+fetch(queryURL)
+.then(function (response) {
+     return response.json();
+ })
+ .then(function(data) {
+     console.log(data);
+ })
+
+ }
 
 
 
-    fetch(queryURL)
-       .then(function (response) {
-            return response.json();
-        })
-        .then(function(data) {
-            console.log(data);
-        })
-    
+// adds event listener for search button
+searchButton.click(searchCity);
+
+
